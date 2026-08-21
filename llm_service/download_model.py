71ecd45b9492feee2e49f804ce8350b2524b95d1
Aurélie SGARD <aurelie.sgard@icloud.com>
2026-08-22 01:46:54 +0200

    Update model repository and filename in download script

--- llm_service/download_model.py
+++ llm_service/download_model.py
@@ -3,7 +3,7 @@ au moment du build de l'image."""
 from huggingface_hub import hf_hub_download
 
 hf_hub_download(
-    repo_id="Qwen/Qwen2.5-0.5B-Instruct-GGUF",
-    filename="qwen2.5-0.5b-instruct-q4_k_m.gguf",
+    repo_id="Qwen/Qwen2.5-1.5B-Instruct-GGUF",
+    filename="qwen2.5-1.5b-instruct-q4_k_m.gguf",
     local_dir="/app/model",
 )
